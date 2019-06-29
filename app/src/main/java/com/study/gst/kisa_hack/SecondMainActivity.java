af87bd6b3c4078b57c80ed32ae5477140a907ce5
djlee <ldj0635@gmail.com>
2019-06-29 17:26:47 +0900

    third button added

---
+++ app/src/main/java/com/study/gst/kisa_hack/SecondMainActivity.java
@@ -0,0 +1,25 @@
+package com.study.gst.kisa_hack;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.study.gst.kisa_hack.fragment.PeopleFragment;
+
+public class SecondMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second_main);
+
+        BottomNavigationView bottomNavigationView = (BottomNavigationView) findViewById(R.id.secondmainactivity_bottomnavigationview);
+
+        getFragmentManager().beginTransaction().replace(R.id.secondmainactivity_framelayout, new PeopleFragment()).commit();
+
+    }
+}
--- app/src/main/java/com/study/gst/kisa_hack/fragment/SecondPeopleFragment.java
+++ app/src/main/java/com/study/gst/kisa_hack/fragment/SecondPeopleFragment.java
@@ -1,9 +1,5 @@
 package com.study.gst.kisa_hack.fragment;
 
-
-
-
-import android.app.Fragment;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
@@ -14,46 +10,40 @@ import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-
 import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.request.RequestOptions;
-import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 import com.study.gst.kisa_hack.FirstReceiveActivity;
 import com.study.gst.kisa_hack.FirstSendActivity;
-import com.study.gst.kisa_hack.LoginActivity;
-import com.study.gst.kisa_hack.MainActivity;
 import com.study.gst.kisa_hack.R;
-import com.study.gst.kisa_hack.SecondMainActivity;
 import com.study.gst.kisa_hack.chat.MessageActivity;
 import com.study.gst.kisa_hack.model.UserModel;
-
-import org.w3c.dom.Text;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class PeopleFragment extends Fragment {
+public class SecondPeopleFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_people, container, false);
-        RecyclerView recyclerView = (RecyclerView)view.findViewById(R.id.peoplefragment_recyclerview);
+        RecyclerView recyclerView = (RecyclerView)view.findViewById(R.id.secondpeoplefragment_recyclerview);
         recyclerView.setLayoutManager(new LinearLayoutManager(inflater.getContext()));
-        recyclerView.setAdapter(new PeopleFragmentRecyclerViewAdapter());
+        recyclerView.setAdapter(new SecondPeopleFragment.PeopleFragmentRecyclerViewAdapter());
         return view;
     }
 
     class PeopleFragmentRecyclerViewAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder>{
         List<UserModel> userModels;
         public PeopleFragmentRecyclerViewAdapter(){
-           userModels = new ArrayList<>();
+            userModels = new ArrayList<>();
             FirebaseDatabase.getInstance().getReference().child("users").addValueEventListener(new ValueEventListener() {
                 @Override
                 public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
@@ -76,7 +66,7 @@ public class PeopleFragment extends Fragment {
         public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
             View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_friend,parent,false);
 
-            return new CustomViewHolder(view);
+            return new SecondPeopleFragment.PeopleFragmentRecyclerViewAdapter.CustomViewHolder(view);
         }
 
         @Override
@@ -85,9 +75,9 @@ public class PeopleFragment extends Fragment {
                     (holder.itemView.getContext())
                     .load(userModels.get(position).profileImageUrl)
                     .apply(new RequestOptions().circleCrop())
-                    .into(((CustomViewHolder)holder).imageView);
-            ((CustomViewHolder)holder).textView.setText(userModels.get(position).userName);
-            ((CustomViewHolder)holder).button1.setOnClickListener(new View.OnClickListener() {
+                    .into(((SecondPeopleFragment.PeopleFragmentRecyclerViewAdapter.CustomViewHolder)holder).imageView);
+            ((SecondPeopleFragment.PeopleFragmentRecyclerViewAdapter.CustomViewHolder)holder).textView.setText(userModels.get(position).userName);
+            ((SecondPeopleFragment.PeopleFragmentRecyclerViewAdapter.CustomViewHolder)holder).button1.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View view) {
                     Intent intent = new Intent(view.getContext(), FirstSendActivity.class);
@@ -95,7 +85,7 @@ public class PeopleFragment extends Fragment {
                     startActivity(intent);
                 }
             });
-            ((CustomViewHolder)holder).button2.setOnClickListener(new View.OnClickListener() {
+            ((SecondPeopleFragment.PeopleFragmentRecyclerViewAdapter.CustomViewHolder)holder).button2.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View view) {
                     Intent intent = new Intent(view.getContext(), FirstReceiveActivity.class);
@@ -103,15 +93,13 @@ public class PeopleFragment extends Fragment {
                     startActivity(intent);
                 }
             });
-            ((CustomViewHolder)holder).button3.setOnClickListener(new View.OnClickListener() {
+            ((SecondPeopleFragment.PeopleFragmentRecyclerViewAdapter.CustomViewHolder)holder).button3.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View view) {
-                    Intent intent = new Intent(view.getContext(), SecondMainActivity.class);
-                    intent.putExtra("email",userModels.get(position).email);
-                    startActivity(intent);
+                    return;
                 }
             });
-            ((CustomViewHolder)holder).button4.setOnClickListener(new View.OnClickListener() {
+            ((SecondPeopleFragment.PeopleFragmentRecyclerViewAdapter.CustomViewHolder)holder).button4.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View view) {
                     return;
